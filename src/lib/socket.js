bf9740c4df6aae7cd4b46e8337047dbee82ff9c3
theHimanshuShekhar <hshekhar.dev@gmail.com>
2024-03-28 16:43:12 +0530

    log value of process.env.NEXT_PUBLIC_WEBSOCKET_SERVER

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
+console.log(`Connecting to ${process.env.NEXT_PUBLIC_WEBSOCKET_SERVER}`)
+
 export const socket = io(process.env.NEXT_PUBLIC_WEBSOCKET_SERVER, {
   autoConnect: false,
 });
