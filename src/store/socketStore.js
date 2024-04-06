17899e30da337e5dc86e595e357316b198b851ad
theHimanshuShekhar <hshekhar.dev@gmail.com>
2024-04-06 08:19:04 +0530

    url for websocket server

--- src/store/socketStore.js
+++ src/store/socketStore.js
@@ -3,7 +3,7 @@
 import { io } from "socket.io-client";
 import { create } from "zustand";
 
-const websocket = io(process.env.NEXT_PUBLIC_WEBSOCKET_SERVER, {
+const websocket = io("https://websocket_streamer.bhayanak.net/", {
   autoConnect: false,
 });
 
